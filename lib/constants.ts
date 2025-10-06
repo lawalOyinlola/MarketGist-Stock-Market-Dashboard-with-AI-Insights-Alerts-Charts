e98c347935cbc483a05bb36746947213c96ffae5
Lawal Oyinlola <oyinlolalawal1705@gmail.com>
2025-10-06 20:40:51 +0000

    implement header

---
+++ lib/constants.ts
@@ -0,0 +1,19 @@
+interface NavItem {
+  href: string;
+  label: string;
+}
+
+export const NAV_ITEMS: NavItem[] = [
+  {
+    href: "/",
+    label: "Dashboard",
+  },
+  {
+    href: "/search",
+    label: "Search",
+  },
+  {
+    href: "/watchlist",
+    label: "Watchlist",
+  },
+];
